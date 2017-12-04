6f4c92cb190b24d7adee061d97d3dd7117c57dae
Wesley Brown <wesley@design-image.co.uk>
2017-12-04 10:53:50 +0000

    Added 404 page template

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<h1>Error 404 - File Not Found.</h1>
+
+<h3>Please <a href="<?php bloginfo('home'); ?>" Click here</a> to return to our home page</h3>
+
+<?php get_footer(); ?>
